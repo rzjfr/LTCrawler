1362198daf5dda52c776684070c7b97a892ebf06
rzjfr <rzjfr@yahoo.com>
2013-06-10 15:52:23 +0430

    find missing isbns from LT

--- books.py
+++ books.py
@@ -18,6 +18,30 @@ def find_isbn_title(title):
         return ''
 
 
+def find_json_name(name):
+    """(str)->str
+    dsc: find json file for given name
+    """
+    url = '''http://www.librarything.com/api_getdata.php?
+userid=%s&tagList=0&showstructure=1&max=1000000&
+reviewmax=10000000&showCollections=1&showReviews=1&showCollections=1
+&showTags=1&responseType=json''' % name
+    url = url.replace("\n", "")
+    try:
+        respond = urlopen(url)
+    except HTTPError, err:
+        if err.code == 404:
+            print "Page not found!"
+        elif err.code == 403:
+            print "Access denied!"
+        else:
+            print "Error ", err.code
+    except URLError, err:
+        print err.reason
+    data = respond.read()
+    return data
+
+
 def find_isbn_name(name):
     """(srt) -> list
     dsc: return all isbn list of a given person
@@ -28,33 +52,27 @@ def find_isbn_name(name):
         with open('./data/profile/'+name+'.json') as file:
             data = file.read()
     except IOError:  # otherwise get it and save it  for further use
-        print 'Not found in database, Retrieving information...'
-        url = '''http://www.librarything.com/api_getdata.php?
-userid=%s&tagList=0&showstructure=1&max=1000000&
-reviewmax=10000000&showCollections=1&showReviews=1&showCollections=1
-&showTags=1&responseType=json''' % name
-        url = url.replace("\n", "")
-        try:
-            respond = urlopen(url)
-        except HTTPError, err:
-            if err.code == 404:
-                print "Page not found!"
-            elif err.code == 403:
-                print "Access denied!"
-            else:
-                print "Error ", err.code
-        except URLError, err:
-            print err.reason
-        data = respond.read()
+        print 'Retrieving data for %s...' % name
+        data = find_json_name(name)
         with open("./data/profile/"+name+".json", "w") as file:
             file.write(data)
     result = []
-    data = json.loads(data)
-    print data['books']['88174801']
-    for book_id in data['books'].keys():
-        result.append(data['books'][book_id]['ISBN_cleaned'])
-    return result
+    if data:
+        data = json.loads(data)
+        if 'books' in data.keys():
+            for book_id in data['books'].keys():
+                isbn = data['books'][book_id]['ISBN_cleaned']
+                if isbn == '':
+                    title = data['books'][book_id]['title']
+                    print 'Retrieving isbn for %s...' % title
+                    isbn = find_isbn_title(title)
+                    result.append(isbn)
+                else:
+                    result.append(isbn)
+        return result
+    else:
+        return 'No data found for %s' % name
 
-#print find_isbn_name("Jon.Roemer")
-title = 'Information Cloud (Tales of Cinnamon City)'
-print find_isbn_title(title)
+print find_isbn_name("Jon.Roemer")
+#title = 'Information Cloud (Tales of Cinnamon City)'
+#print find_isbn_title(title)
