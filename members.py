5bd85dbfb58c5e25ff6c83ee967713edd3afacb7
rzjfr <rzjfr@yahoo.com>
2013-06-11 01:15:23 +0430

    retrieve and save member page for given book

--- members.py
+++ members.py
@@ -13,3 +13,29 @@ def log(message):
     with open("./data/members.log", "a") as file:
         file.write(date+" "+message+"\n")
     print("Error: "+message+", more information in book.log")
+
+
+def get_members_work(work):
+    """(str)->str
+    dsc: returns a html string of a book memebers
+    """
+    print 'Retrieving data for %s...' % work
+    url = "http://www.librarything.com/ajaxinc_userswithabook.php?work=" + work
+    try:
+        html = urlopen(url)
+    except HTTPError, err:
+        if err.code == 404:
+            log("Page not found!")
+        elif err.code == 403:
+            log("Access denied!")
+        else:
+            log("Error "+str(err.code))
+    except URLError, err:
+        log(str(err.reason))
+    result = html.read()
+    # save the result
+    with open('./data/book/'+work+'.html', 'w') as data:
+        data.write(result)
+    return result
+
+#print get_members_work('12569876')
