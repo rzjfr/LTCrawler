fd45bc3cb9bbe70c70239ce394013cce66f432c2
rzjfr <rzjfr@yahoo.com>
2013-06-11 01:55:59 +0430

    add logging functionality for friends madule

---
+++ friends.py
@@ -0,0 +1,15 @@
+import json
+from urllib2 import *
+from BeautifulSoup import BeautifulSoup
+from datetime import datetime
+from time import sleep
+
+
+def log(message):
+    """(str)->None
+    dsc: logging
+    """
+    date = str(datetime.now())
+    with open("./data/friends.log", "a") as file:
+        file.write(date+" "+message+"\n")
+    print("Error: "+message+", more information in book.log")
