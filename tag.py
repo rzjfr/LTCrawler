91911c906c657fa05ee8f5096d85039962794daf
rzjfr <rzjfr@yahoo.com>
2013-06-11 10:06:41 +0430

    get all tags with count for given book

--- tag.py
+++ tag.py
@@ -15,4 +15,34 @@ def log(message):
     print("Error: "+message+", more information in tags.log")
 
 
+def get_all(work):
+    """(str)->dic
+    dsc: get all tags and counts for given book
+    """
+    url = """http://www.librarything.com/ajaxinc_showbooktags.php?work=%s
+&all=1&print=1&doit=1&lang=en""" % work
+    url = url.replace("\n", "")
+    try:
+        html = urlopen(url)
+    except HTTPError, err:
+        if err.code == 404:
+            log("Page not found!")
+        elif err.code == 403:
+            log("Access denied!")
+        else:
+            log("Error "+str(err.code))
+    except URLError, err:
+        log(str(err.reason))
+    html = BeautifulSoup(html.read())
+    result = {}
+    tags = html.findAll('span', attrs={'class': 'tag'})
+    for tag in tags:
+        name = tag.find('a').text
+        count = tag.find('span', attrs={'class': 'count'}).text[1:-1]
+        if name in result:
+            log('tag %s from %s overwited' % (name, work))
+        result.update({name: count})
+    return result
 
+
+print get_all('306947')
