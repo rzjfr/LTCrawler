53e0112546aa41a26c4c46a9dd5dbc4480d9735f
rzjfr <rzjfr@yahoo.com>
2013-06-17 22:07:58 +0430

    find all friends of given book members

--- find_friends_book.py
+++ find_friends_book.py
@@ -25,7 +25,7 @@ def all_local_members():
         for line in name_repository:
             record = json.loads(line)
             result.extend(record.keys())
-            if record.values() != []:
+            if record.values()[0].__class__ == list:
                 result.extend(record.values()[0])
     result = list(set(result))
     return result
@@ -47,32 +47,61 @@ def find_friends_recursive(members, known):
     dsc: gets all members of a given book and returns friends
     for each one recursively
     """
-    for name in members:
-        print "find friends for %s" % name
-        all_friends = friends.find_friends(name)
-        if all_friends != []:
-            if name not in known:
-                print "continue with %s" % str(all_friends)
-                known.update({name: all_friends})
-                find_friends_recursive(all_friends, known)
-            #else:
-                #for friend in all_friends:
-                    #if friend not in known:
-                        #found_friends = friends.find_friends(friend)
-                        #known.update({friend: found_friends})
-                        #find_friends_recursive(found_friends, known)
-        else:
-            print 'done for %s' % name
-    print "finished successfully!"
+    i = 0
+    print members
+    if members.__class__ == list:
+        for name in members:
+            i += 1
+            print '%d of %d' % (i, len(members))
+            print "find friends for %s" % name
+            all_friends = friends.find_friends(name)
+            if all_friends.__class__ == list and all_friends:
+                if name not in known:
+                    print "continue with %s" % str(all_friends)
+                    known.update({name: all_friends})
+                    find_friends_recursive(all_friends, known)
+            else:
+                print 'done for %s' % name
+        print "finished successfully! %d" % len(members)
 
+
+def find_friends_bfs(members, known):
+    """(list, dict)->list
+    dsc: find first level of friends and return known stage
+    """
+    i = 0
+    users_to_find_next = []
+    if members.__class__ == list:
+        for name in members:
+            i += 1
+            print '%d of %d, find friends for %s' % (i, len(members), name)
+            all_friends = friends.find_friends(name)
+            if all_friends.__class__ == list and all_friends:
+                users_to_find_next.extend(all_friends)
+    return list(set(users_to_find_next))
 
 if __name__ == '__main__':
     #title = sys.argv[1]
-    title = 'The Holy Bible: King James Version (KJV)'
-    work = books.get_work_title_retry(title)
-    members = members.find_all_members(work[:-1])
+    #work = books.get_work_title_retry(title)
+    #members = members.find_all_members(work[:-1])
+    #title = 'The Holy Bible: King James Version (KJV)'
+    #title = 'The Blind Watchmaker'
+    #title = 'Mere Christianity'
+    #title = 'The Holy Bible: Revised Standard Version (RSV)'
+    #title = 'The Holy Bible: New King James Version (NKJV)'
+    #title = 'The Holy Bible: New American Standard (NASB)'
+    #title = 'The God Delusion'
+    #work = '1576656'
+    #work = '306947'
+    #work = '1595966'
+    #work = '324481'
+    #work = '5503022'
+    #work = '5503098'
+    #work = '1429542'
+    #members = members.find_all_members(work)  # all members of given work
     known = load_local_friends()  # all known usernames
-    find_friends_recursive(members, known)
+    #users = find_friends_bfs(members, known)
+    #find_friends_recursive(members, known)
     all_members = all_local_members()
     find_missing_friends(all_members, known)
-    books.log('%s finished completely' % title)
+    #books.log('%s finished completely' % title)
--- friends.py
+++ friends.py
@@ -35,20 +35,24 @@ def get_all_friends(name):
     except HTTPError, err:
         if err.code == 404:
             log("Page not found!")
+            return 'No access'
         elif err.code == 403:
             log("Access denied!")
+            return 'No access'
         else:
             log("Error "+str(err.code))
+            return 'No access'
     except URLError, err:
         log(str(err.reason))
+        return 'No access'
     if not friends:
         alert = html.find('p', attrs={'class': 'alert'})
-        profile = html.find('div', attrs={'class': 'memberprofile'})
+        profile = html.find('div', attrs={'class': 'profile'})
         if alert:
             log('No data for %s: %s' % (name, alert.text[:-1]))
             return 'Removed'
         elif profile:
-            log('No data for %s: user data is private')
+            log('No data for %s: user data is private' % name)
             return 'Private'
         else:
             alert = html.find('p')
@@ -69,6 +73,9 @@ def get_all_friends(name):
         if friends.find('b').text == 'Friends:':
             for friend in friends('a'):
                 result.append(friend.text)
+        else:
+            log('No data for %s: No list is available' % name)
+            return 'No list'
     return result
 
 
@@ -95,7 +102,9 @@ def find_friends(name):
 #print get_all_friends('Mysterion')
 #print get_all_friends('razorsoccamsells')
 #print get_all_friends('vchia')
+#print get_all_friends('newsativa')
 #print find_friends('Movielizard')
 #print find_friends('gothic_cowgirl')
 #print get_all_friends('CLHarris')
-#print get_all_friends('sfreads')
+#print get_all_friends('aakin')
+#print get_all_friends('DiannaN')
