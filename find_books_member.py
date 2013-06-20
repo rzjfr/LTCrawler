3e6dd05626751de3b81e6a00e7af4344949c08e7
rzjfr <rzjfr@yahoo.com>
2013-06-20 12:42:53 +0430

    find book list for members of given work id in html format

--- find_books_member.py
+++
@@ -1,9 +0,0 @@
-import books
-import json
-from HelperMethods import *
-
-
-members = load_local_friends()
-for member in members:
-    print member
-    books.find_books(member)
---
+++ find_books_user_html.py
@@ -0,0 +1,18 @@
+import books
+import json
+import sys
+from HelperMethods import *
+
+try:
+    work = sys.argv[1]
+    #members = load_local_friends()
+except:
+    work = '306947'
+members = books.find_all_members(work)  # all members of given work
+members = remove_duplicate(members)
+print len(members)
+i = 0
+for member in members:
+    i += 1
+    print '%d of %d, %s' % (i, len(members), member)
+    books.find_books(member)
